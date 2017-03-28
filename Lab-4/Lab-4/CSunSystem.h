ff8089c85c42437b647adb8ec265ab71c01d49d5
Dima Shumanski <dima-shumanski@yandex.ru>
2017-03-28 22:35:50 +0300

    Update Lab-4 — added Jupiter

--- Lab-4/Lab-4/CSunSystem.h
+++ Lab-4/Lab-4/CSunSystem.h
@@ -3,47 +3,55 @@ CMatrix CreateRotate2D(double);
 
 class CSunSystem
 {
+private:
 	CRect Sun;		  // Прямоугольники планет
 	CRect Earth;
 	CRect Moon;
-	CRect Venus;
-	CRect Mercury;
-	CRect Mars;
+	CRect Venus,
+		  Mercury,
+		  Mars,
+		  Jupiter;
 	CRect EarthOrbit; // Прямоугольники, описанные вокруг орбиты планет
 	CRect MoonOrbit;
-	CRect VenusOrbit;
-	CRect MercuryOrbit;
-	CRect MarsOrbit;
+	CRect VenusOrbit,
+		  MercuryOrbit,
+		  MarsOrbit,
+		  JupiterOrbit;
 	CMatrix ECoords;  // Текущие координаты Земли в СК Солнца ( x,y,1)
-	CMatrix VCoords;
-	CMatrix MeCoords;
-	CMatrix MaCoords;
+	CMatrix VCoords,
+		    MeCoords,
+		    MaCoords,
+		    JCoords;
 	CMatrix MCoords;  // Текущие координаты Луны в СК Земли ( x,y,1)
 	CMatrix MCoords1; // Текущие координаты Луны в СК Солнца ( x,y,1)
 	CMatrix PM;		  // Матрицы попорота
 	CMatrix PE;
-	CMatrix PV;
-	CMatrix PMe;
-	CMatrix PMa;
+	CMatrix PV,
+		    PMe,
+		    PMa,
+		    PJ;
 	CRect RW;		  // Прямоугольник в окне
 	CRectD RS;		  // Прямоугольник области в МСК
 	double wEarth;	  // Угловая скорость палнет
 	double wMoon;
-	double wVenus;
-	double wMercury;
-	double wMars;
+	double wVenus,
+		   wMercury,
+		   wMars,
+		   wJupiter;
 	double fiE;		  // Угловое положение Земли в системе кординат Солнца, град
 	double dfiE;	  // Угол поворота Земли за время dt.
-	double fiV;
-	double dfiV;
-	double fiMe;
-	double dfiMe;
-	double fiMa;
-	double dfiMa;
 	double fiM;		  // Угловое положение Луны в системе кординат Земли, град
 	double dfiM;	  // Угол поворота Земли за время dt
-
+	double fiV,
+		   dfiV,
+		   fiMe,
+		   dfiMe,
+		   fiMa,
+		   dfiMa,
+		   fiJ,
+		   dfiJ;
 	double dt;		  // Интервал дискретизации, сек.
+
 public:
 	CSunSystem();
 	void SetDT(double dtx){ dt = dtx; }; // Установка интервала дискретизации
@@ -56,40 +64,46 @@ public:
 CSunSystem::CSunSystem()
 {
 	double rS = 35, rE = 20, rM = 5,                  // Радиусы планет
-		rV = 23, rMe = 15, rMa = 17;              
+		rV = 23, rMe = 9, rMa = 17,
+		rJ = 30;
 	double RoE = 7.5 * rS, RoM = 2 * rE,              // Радиусы орбит
-		RoV = 6 * rV, RoMe = 5.5 * rMe,
-		RoMa = 21 * rMa;
+		RoV = 6 * rV, RoMe = 8 * rMe,
+		RoMa = 21 * rMa, RoJ = 17 * rJ;
 	double d = RoE + RoM + rM + RoMa;
+
 	RS.SetRectD(-d, d, d, -d);                        // Обалсть системы в МСК
 	RW.SetRect(0, 0, 650, 650);                       // Область системы в окне
+
 	Sun.SetRect(-rS, rS, rS, -rS);                    // Прямоуольники планет
 	Earth.SetRect(-rE, rE, rE, -rE);
 	Moon.SetRect(-rM, rM, rM, -rM);
 	Venus.SetRect(-rV, rV, rV, -rV);
 	Mercury.SetRect(-rMe, rMe, rMe, -rMe);
 	Mars.SetRect(-rMa, rMa, rMa, -rMa);
+	Jupiter.SetRect(-rJ, rJ, rJ, -rJ);
 	EarthOrbit.SetRect(-RoE, RoE, RoE, -RoE);         // Прямоугольники орбит
 	MoonOrbit.SetRect(-RoM, RoM, RoM, -RoM);
 	VenusOrbit.SetRect(-RoV, RoV, RoV, -RoV);
 	MercuryOrbit.SetRect(-RoMe, RoMe, RoMe, -RoMe);
 	MarsOrbit.SetRect(-RoMa, RoMa, RoMa, -RoMa);
-	fiE = 0;                                          // Уловое положение
-	fiM = 0;
-	fiV = 0;
-	fiMe = 0;
-	fiMa = 0;
+	JupiterOrbit.SetRect(-RoJ, RoJ, RoJ, -RoJ);
+
+	fiE = 0; fiM = 0;                                  // Уловое положение 
+	fiV = 0; fiMe = 0;
+	fiMa = 0; fiJ = 0;
 	wEarth = 15;                                       // Угловая скорость
 	wMoon = 170;
 	wVenus = 20;
 	wMercury = 23;
 	wMars = 11;
+	wJupiter = 6;
 	dt = 0.2;
 	dfiM = wMoon * dt;
 	dfiE = wEarth * dt;
 	dfiV = wVenus * dt;
 	dfiMe = wMercury * dt;
 	dfiMa = wMars * dt;
+	dfiJ = wJupiter * dt;
 
 	MCoords.RedimMatrix(3);
 	MCoords1.RedimMatrix(3);
@@ -97,47 +111,51 @@ CSunSystem::CSunSystem()
 	VCoords.RedimMatrix(3);
 	MeCoords.RedimMatrix(3);
 	MaCoords.RedimMatrix(3);
+	JCoords.RedimMatrix(3);
 	PE.RedimMatrix(3, 3);
 	PM.RedimMatrix(3, 3);
 	PV.RedimMatrix(3, 3);
 	PMe.RedimMatrix(3, 3);
 	PMa.RedimMatrix(3, 3);
+	PJ.RedimMatrix(3, 3);
 
 	RoM = (MoonOrbit.right - MoonOrbit.left)   / 2; // Радиус орбит
 	RoE = (EarthOrbit.right - EarthOrbit.left) / 2;
 	RoV = (VenusOrbit.right - VenusOrbit.left) / 2;
 	RoMe = (MercuryOrbit.right - MercuryOrbit.left) / 2;
 	RoMa = (MarsOrbit.right - MarsOrbit.left) / 2;
+	RoJ = (JupiterOrbit.right - JupiterOrbit.left) / 2;
 	double ff = (fiM / 180.0) * pi;                 // Радианы
 	double x = RoM * cos(ff);
 	double y = RoM * sin(ff);
 	MCoords(0) = x; MCoords(1) = y; MCoords(2) = 1;
 
-	ff = (fiE / 180.0) * pi;
 	x = RoE * cos(ff);
 	y = RoE * sin(ff);
 	ECoords(0) = x; ECoords(1) = y; ECoords(2) = 1;
 
-	ff = (fiE / 180.0) * pi;
 	x = RoV * cos(ff);
 	y = RoV * sin(ff);
 	VCoords(0) = x; VCoords(1) = y; VCoords(2) = 1;
 
-	ff = (fiE / 180.0) * pi;
 	x = RoMe * cos(ff);
 	y = RoMe * sin(ff);
 	MeCoords(0) = x; MeCoords(1) = y; MeCoords(2) = 1;
 
-	ff = (fiE / 180.0) * pi;
 	x = RoMa * cos(ff);
 	y = RoMa * sin(ff);
 	MaCoords(0) = x; MaCoords(1) = y; MaCoords(2) = 1;
+
+	x = RoJ * cos(ff);
+	y = RoJ * sin(ff);
+	JCoords(0) = x; JCoords(1) = y; JCoords(2) = 1;
 
 	PE = CreateRotate2D(dfiE);
 	PM = CreateRotate2D(dfiM);
 	PV = CreateRotate2D(dfiV);
 	PMe = CreateRotate2D(dfiMe);
 	PMa = CreateRotate2D(dfiMa);
+	PJ = CreateRotate2D(dfiJ);
 }
 
 void CSunSystem::SetNewCoords()
@@ -153,11 +171,13 @@ void CSunSystem::SetNewCoords()
 	VCoords = PV * VCoords;
 	MeCoords = PMe * MeCoords;
 	MaCoords = PMa * MaCoords;
+	JCoords = PJ * JCoords;
 }
 
 void CSunSystem::Draw(CDC &dc)
 {
-	CBrush SBrush, EBrush, MBrush, VBrush, MeBrush, MaBrush, *pOldBrush;
+	CBrush SBrush, EBrush, MBrush, *pOldBrush,
+		VBrush, MeBrush, MaBrush, JBrush;
 	CRect R;
 
 	SBrush.CreateSolidBrush(RGB(255, 69, 0));
@@ -166,6 +186,7 @@ void CSunSystem::Draw(CDC &dc)
 	VBrush.CreateSolidBrush(RGB(205, 133, 63));
 	MeBrush.CreateSolidBrush(RGB(160, 82, 45));
 	MaBrush.CreateSolidBrush(RGB(255, 0, 0));
+	JBrush.CreateSolidBrush(RGB(244, 164, 96));
 
 	// Рисуем орбиты
 	dc.SelectStockObject(NULL_BRUSH);
@@ -173,6 +194,7 @@ void CSunSystem::Draw(CDC &dc)
 	dc.Ellipse(VenusOrbit);
 	dc.Ellipse(MercuryOrbit);
 	dc.Ellipse(MarsOrbit);
+	dc.Ellipse(JupiterOrbit);
 
 	int d = MoonOrbit.right;
 	R.SetRect(ECoords(0) - d, ECoords(1) + d, ECoords(0) + d, ECoords(1) - d);
@@ -210,6 +232,12 @@ void CSunSystem::Draw(CDC &dc)
 	d = Mars.right;
 	R.SetRect(MaCoords(0) - d, MaCoords(1) + d, MaCoords(0) + d, MaCoords(1) - d);
 	dc.SelectObject(&MaBrush);
+	dc.Ellipse(R);
+
+	// Рисуем Юпитер
+	d = Jupiter.right;
+	R.SetRect(JCoords(0) - d, JCoords(1) + d, JCoords(0) + d, JCoords(1) - d);
+	dc.SelectObject(&JBrush);
 	dc.Ellipse(R);
 
 	dc.SelectObject(pOldBrush);
